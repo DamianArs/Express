ff340bb19381c74178f46be7f080ad22775ed236
DamianArs <damian.arsenicz@gmail.com.pl>
2019-11-05 22:22:20 +0100

    second

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var createError = require("http-errors");
+var cookieSession = require('cookie-session')
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
@@ -26,8 +27,14 @@ app.use(
 );
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(cookieSession({
+  name: 'session',
+  keys: [ /* secret keys */ ],
 
-app.use(function(req, res, next) {
+  // Cookie Options
+  maxAge: 24 * 60 * 60 * 1000 // 24 hours
+}))
+app.use(function (req, res, next) {
   res.locals.path = req.path;
   next();
 });
@@ -40,12 +47,12 @@ app.use("/admin", adminRouter);
 app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
---
+++ routes/config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    keySession: []
+}
